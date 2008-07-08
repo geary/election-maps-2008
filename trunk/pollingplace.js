35b6d0473824db5ad8d5ae4500f75b0a3ac8541b
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-07-08 07:37:05 +0000

    Set gadget height on when Search clicked

--- trunk/pollingplace.js
+++ trunk/pollingplace.js
@@ -1,1 +1,8 @@
-alert('hi');
+(function() {
+	
+	function e( id ) { return document.getElementById('PollingPlaceSearch'+id); }
+	var wrapper = e('Wrapper'), spinner = e('Spinner'), input = e('Input');
+	
+	wrapper.style.height = wrapper.offsetHeight + 400 + 'px';
+	
+})();
