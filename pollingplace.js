728af8fb863fb88163f07989e38ca693926c526a
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-07-08 07:41:51 +0000

    Disable search button after clicking

--- pollingplace.js
+++ pollingplace.js
@@ -1,8 +1,10 @@
 (function() {
 	
 	function e( id ) { return document.getElementById('PollingPlaceSearch'+id); }
-	var wrapper = e('Wrapper'), spinner = e('Spinner'), input = e('Input');
+	var wrapper = e('Wrapper'), spinner = e('Spinner'), input = e('Input'), button = e('Button');
 	
+	button.disabled = true;
+	button.blur();
 	wrapper.style.height = wrapper.offsetHeight + 400 + 'px';
 	
 })();
