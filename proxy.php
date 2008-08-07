884e95d4d398e3df747e5d6cacb3c5259e3dc135
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-08-07 05:34:08 +0000

    Minor proxy update

--- proxy.php
+++ proxy.php
@@ -3,7 +3,11 @@
 $url = 'http://pollinglocation.apis.google.com/?q=' . urlencode($_GET['q']);
 
 $session = curl_init($url);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($session);
+
+echo $response;
+
 curl_close($session);
 
 ?>
