a26718ec9b1a4cdfa20e292bc4ab6b4e6a21acca
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-08-14 05:50:44 +0000

    Implement mapplet

--- pollingplace-frame.js
+++ pollingplace-frame.js
@@ -1,6 +1,9 @@
+mapplet = window.mapplet;
+
 var key = {
 	'gigapad': 'ABQIAAAAgNQJhbWKFHRJJiHCXotPZxTCDaeoAnk9GZSdGi854AcXbJXoXRS9QqxqDWHL54Twi5thIIANaCUAeA',
 	'gigapad.local': 'ABQIAAAAgNQJhbWKFHRJJiHCXotPZxTM3oEyKJfFfMe3pRkahPyHmmHL_xSjOVWnaIMs0gtUrFuoOJzsQiPnKA',
+	'maps.gmodules.com': 'ABQIAAAAL7MXzZBubnPtVtBszDCxeRTqV_PyyxRizIwUkTfU6T-V7M7btRRpOM29SpcTDh2dojFpfRwpoTTMWw',
 	'mg.to': 'ABQIAAAAL7MXzZBubnPtVtBszDCxeRQjDwyFD3YNj60GgWGUlJCU_q5i9hSSSzj0ergKKMY55eRpMa05FE3Wog',
 	'padlet': 'ABQIAAAAL7MXzZBubnPtVtBszDCxeRSuIOvo2pGs7VouOPMgaUBd9TDiaBTE5gjPTrifBPED7VUFoeKD_Ysmkw',
 	'padlet.local': 'ABQIAAAAgNQJhbWKFHRJJiHCXotPZxTbToHSggDWprRoD6gaXq5geEyxiBTRRmW6BwPHdCzJ2mh90uajjkpAOA',
@@ -9,9 +12,65 @@ var key = {
 }[location.host];
 
 document.write(
-	'<script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=', key, '">',
-	'<\/script>'
+	'<style type="text/css">',
+		'body.gadget { margin:0; padding:0; }',
+		'body { font-family:Arial,sans-serif; font-size:10pt; }',
+		'#spinner { z-index: 1; position:absolute; width:100%; height:100%; background-image:url(spinner.gif); background-position:center; background-repeat:no-repeat; opacity:0.30; -moz-opacity:0.30; }',
+		'#spinner { filter:alpha(opacity=30); }',
+		'#title { margin-bottom:4px; }',
+		'#title, #map { overflow: auto; }',
+	'</style>'
 );
+
+if( mapplet ) {
+	document.write(
+		'<style type="text/css">',
+			'#PollingPlaceSearch { font-size:14px; width:100%; margin:0; padding:0; }',
+			'.PollingPlaceSearchForm { margin:0; padding:0; }',
+			'.PollingPlaceSearchTitle { float:left; /*font-weight:bold;*/ font-size:120%; padding-bottom:4px; }',
+			'/*.PollingPlaceSearchSpinner { float:right; margin-right:4px; width:16px; height:16px; background-image:url(spinner16.gif); background-position:1000px 0px; background-repeat:no-repeat; }*/',
+			'.PollingPlaceSearchLabelBox { position:relative; float:left; margin-right:6px; }',
+			'.PollingPlaceSearchLabel { position:absolute; left:5px; top:5px; z-index:1; color:#777; cursor: text; }',
+			'.PollingPlaceSearchInput { width:18.5em; }',
+			'.PollingPlaceSearchButtonBox { float:left; }',
+			'.PollingPlaceSearchButton { width:4em; }',
+			'.PollingPlaceSearchFrameHider { width:100%; height:400px; padding:4px; display:none; }',
+			'.PollingPlaceSearchFrameBox { width:100%; height:100%; }',
+		'</style>',
+		'<div id="PollingPlaceSearch">',
+			'<div class="PollingPlaceSearchTitle">',
+				'Enter your <strong>home</strong> address:',
+			'</div>',
+			'<!--<div id="PollingPlaceSearchSpinner" class="PollingPlaceSearchSpinner">-->',
+			'<!--</div>-->',
+			'<div style="clear:both;">',
+			'</div>',
+			'<form id="PollingPlaceSearchForm" class="PollingPlaceSearchForm">',
+				'<div class="PollingPlaceSearchLabelBox">',
+					'<label id="PollingPlaceSearchLabel" for="PollingPlaceSearchInput" class="PollingPlaceSearchLabel">',
+						'1600 Pennsylvania Ave, Washington, DC',
+					'</label>',
+					'<input id="PollingPlaceSearchInput" class="PollingPlaceSearchInput" type="text" onfocus="PollingPlaceSearch.focus()" onblur="PollingPlaceSearch.blur()" />',
+				'</div>',
+				'<div class="PollingPlaceSearchButtonBox">',
+					'<button type="submit" id="PollingPlaceSearchButton" class="PollingPlaceSearchButton" onclick="return PollingPlaceSearch.submit()"> Search</button>',
+				'</div>',
+			'</form>',
+			'<div style="clear:left;">',
+			'</div>',
+			'<div id="PollingPlaceSearchFrameHider" class="PollingPlaceSearchFrameHider">',
+				'<div id="PollingPlaceSearchFrameBox" class="PollingPlaceSearchFrameBox">',
+				'</div>',
+			'</div>',
+		'</div>'
+	);
+}
+else {
+	document.write(
+		'<script type="text/javascript" src="http://maps.google.com/maps?file=api&amp;v=2&amp;key=', key, '">',
+		'<\/script>'
+	);
+}
 
 $(function() {
 
@@ -58,57 +117,63 @@ function url( base, params, delim ) {
 	return a.length ? [ base, a.sort().join('&') ].join( delim || '?' ) : base;
 };
 
-function jsmap( a, link ) {
+function loadMap( a ) {
 	GBrowserIsCompatible() && setTimeout( function() {
-		var $jsmap = $('#jsmap');
-		var map = new GMap2( $jsmap[0], {
+		a.$jsmap = $('#jsmap');
+		var map = new GMap2( a.$jsmap[0], {
 			mapTypes: [
 				G_NORMAL_MAP,
 				G_SATELLITE_MAP,
 				G_SATELLITE_3D_MAP
 			]
 		} );
-		// Initial position with marker centered
-		var latlng = new GLatLng( a.lat, a.lng );
-		map.setCenter( latlng, a.zoom );
+		initMap( a, map );
+	}, 1 );
+}
+
+function initMap( a, map ) {
+	// Initial position with marker centered
+	var latlng = new GLatLng( a.lat, a.lng ), center = latlng;
+	map.setCenter( latlng, a.zoom );
+	if( ! mapplet ) {
 		// Move map down a bit
 		var point = map.fromLatLngToDivPixel( latlng );
 		point = new GPoint(
-			point.x /*- $jsmap.width() * .075*/,
-			point.y - $jsmap.height() * .25
+			point.x /*- a.$jsmap.width() * .075*/,
+			point.y - a.$jsmap.height() * .25
 		);
 		map.setCenter( map.fromDivPixelToLatLng(point), a.zoom );
 		map.addControl( new GSmallMapControl );
 		map.addControl( new GMapTypeControl );
-		var icon = new GIcon( G_DEFAULT_ICON );
-		//icon.image = 'marker-green.png';
-		var marker = new GMarker( latlng, { icon:icon } );
-		map.addOverlay( marker );
-		var html = S(
-			'<div>',
-				'<div style="font-weight:bold;">',
-					'Your Voting Place',
-				'</div>',
-				'<div>',
-					a.street,
-				'</div>',
-				'<div>',
-					a.city, ', ', a.state, ' ', a.zip,
-				'</div>',
-				'<div>',
-					link,
-				'</div>',
-			'</div>'
-		);
-		marker.bindInfoWindowHtml( html );
-		marker.openInfoWindowHtml( html );
-		spin( false );
-	}, 1 );
-	
-	return S(
-		'<div id="jsmap" style="width:', a.width - 6, 'px; height:', a.height, 'px;">',
+	}
+	var icon = new GIcon( G_DEFAULT_ICON );
+	//icon.image = 'marker-green.png';
+	var marker = new GMarker( latlng, { icon:icon } );
+	map.addOverlay( marker );
+	var largeMapLink = mapplet ? '' : S(
+		'<div>',
+			'<a target="_blank" href="http://maps.google.com/maps?f=q&hl=en&geocode=&q=', encodeURIComponent( a.address.replace( / /g, '+' ) ), '&ie=UTF8&ll=', latlng, '&z=15&iwloc=addr">',
+				'Large map and directions',
+			'</a>',
 		'</div>'
 	);
+	var html = S(
+		'<div>',
+			'<div style="font-weight:bold;">',
+				'Your Voting Place',
+			'</div>',
+			'<div>',
+				a.street,
+			'</div>',
+			'<div>',
+				a.city, ', ', a.state, ' ', a.zip,
+			'</div>',
+			largeMapLink,
+		'</div>'
+	);
+	marker.bindInfoWindowHtml( html );
+	marker.openInfoWindowHtml( html );
+	spin( false );
 }
 
 function spin( yes ) {
@@ -129,13 +194,12 @@ function lookup( address, callback ) {
 	//	'http://pollinglocation.apis.google.com/?q=',
 	//	encodeURIComponent(address), '&callback=?'
 	//);
-	var url = S( 'proxy.php?q=', encodeURIComponent(address) );
+	var url = S( 'http://mg.to/elections/proxy.php?callback=?&q=', encodeURIComponent(address) );
 	
 	$.getJSON( url, callback );
 }
 
-function submit() {
-	var addr = decodeURIComponent( location.hash.slice(1) );
+function submit( addr ) {
 	$title.empty();
 	$map.empty();
 	
@@ -170,15 +234,23 @@ function findPrecinct( place ) {
 			else setMap( places[0] );
 		});
 	});
-	
-	function sorry() {
-		$map.html( '<strong>Sorry, we did not find a polling place for this address.</strong>' );
-		spin( false );
+}
+
+function sorry() {
+	$map.html( '<strong>Sorry, we did not find a polling place for this address.</strong>' );
+	spin( false );
+}
+
+function setMap( place ) {
+	var a = mapInfo( place );
+	if( mapplet ) {
+		initMap( a, new GMap2 );
 	}
-	
-	function setMap( place ) {
-		$map.height( $window.height() - $map.offset().top );
-		$map.html( formatMap(place) );
+	else {
+		a.width = $map.width();
+		$map.height( a.height = $window.height() - $map.offset().top );
+		$map.html( formatMap(a) );
+		loadMap( a );
 	}
 }
 
@@ -214,38 +286,65 @@ function formatPlaces( places ) {
 	);
 }
 
-function formatMap( place ) {
-	var address = place.address.replace( /, USA$/, '' );
+function mapInfo( place ) {
 	var area = place.AddressDetails.Country.AdministrativeArea;
 	var sub = area.SubAdministrativeArea || area;
 	var locality = sub.Locality;
 	var coord = place.Point.coordinates;
-	var a = {
+	return {
+		address: place.address.replace( /, USA$/, '' ),
 		lat: coord[1],
 		lng: coord[0],
 		street: locality.Thoroughfare.ThoroughfareName,
 		city: locality.LocalityName,
 		state: area.AdministrativeAreaName,
 		zip: locality.PostalCode.PostalCodeNumber,
-		width: $map.width(),
-		height: $map.height(),
 		zoom: 15,
 		_:''
-	}
-	var latlng = [ a.lat, a.lng ].join();
-	return jsmap( a, link('Large map and directions') );
+	};
+}
 
-	function link( html ) {
-		return S(
-			'<a target="_blank" href="http://maps.google.com/maps?f=q&hl=en&geocode=&q=', encodeURIComponent( address.replace( / /g, '+' ) ), '&ie=UTF8&ll=', a.latlng, '&z=15&iwloc=addr">',
-				html,
-			'</a>'
-		);
-	}
+function formatMap( a ) {
+	return S(
+		'<div id="jsmap" style="width:', a.width - 6, 'px; height:', a.height, 'px;">',
+		'</div>'
+	);
 }
 
 var $window = $(window), $title = $('#title'), $map = $('#map'), $spinner = $('#spinner');
-	
-submit();
+
+if( mapplet ) {
+	(function() {
+		function e( id ) { return document.getElementById('PollingPlaceSearch'+id); }
+		var /*spinner = e('Spinner'),*/ label = e('Label'), input = e('Input'), button = e('Button'), hider = e('FrameHider');
+		button.disabled = false;
+		
+		window.PollingPlaceSearch = {
+			
+			focus: function() {
+				label.style.textIndent = '-1000px';
+			},
+			
+			blur: function() {
+				if( input.value === '' )
+					label.style.textIndent = '0px';
+			},
+			
+			submit: function() {
+				//spinner.style.backgroundPosition = '0px 0px';
+				hider.style.display = 'block';
+				submit( input.value );
+				return false;
+			}
+			
+		};
+		
+		PollingPlaceSearch.focus();
+		PollingPlaceSearch.blur();
+	})();
+}
+else {
+	submit( decodeURIComponent( location.hash.slice(1) ) );
+}
 
 });
--- proxy.php
+++ proxy.php
@@ -6,7 +6,7 @@ $session = curl_init($url);
 curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($session);
 
-echo $response;
+echo $_GET['callback'] . '(' . $response . ')';
 
 curl_close($session);
 
