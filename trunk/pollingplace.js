cb7b36d0fad51d0b92d338f5e3a6213fed76d2b4
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-08-18 00:18:25 +0000

    Fix that base URL again

--- trunk/pollingplace.js
+++ trunk/pollingplace.js
@@ -8,7 +8,7 @@ setTimeout( function() {
 	else if( host in { 's.mg.to':1 } )
 		var base = 'http://' + host + '/elections/', cache = '?' + (+new Date);
 	else
-		var base = 'http://s.mg.to/elections/pollingplace.js', cache = '';
+		var base = 'http://s.mg.to/elections/', cache = '';
 	
 	document.getElementById('PollingPlaceSearchFrameBox').innerHTML = [
 		'<iframe ',
