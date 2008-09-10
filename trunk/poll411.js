7f3558c7c6a7e9fb89ba8ceb053efa418e5a193a
mg@mg.to <mg@mg.to@a793c9aa-4d51-0410-af08-d18108c47ed6>
2008-09-10 08:49:10 +0000

    Get gadget test working again

--- trunk/poll411.js
+++ trunk/poll411.js
@@ -2,18 +2,9 @@ setTimeout( function() {
 	
 	var addr = encodeURIComponent( document.getElementById('PollingPlaceSearchInput').value );
 	
-	var host = location.host;
-	if( host in { 'gigapad':1, 'gigapad.local':1, 'padlet':1, 'padlet.local':1 } )
-		var base = 'http://' + host + '/election-general/', cache = '?' + (+new Date);
-	else if( host in { 's.mg.to':1 } )
-		var base = 'http://' + host + '/elections/', cache = '?' + (+new Date);
-	else
-		var base = 'http://s.mg.to/elections/', cache = '';
-	
 	document.getElementById('PollingPlaceSearchFrameBox').innerHTML = [
 		'<iframe ',
-			'src="', base, 'pollingplace-frame.html', cache, '#', addr, '" ',
-			'id="PollingPlaceSearchResultFrame" ',
+			'src="http://www.gmodules.com/ig/ifr?url=http://election-maps-2008.googlecode.com/svn/trunk/poll411-frame.xml&amp;synd=open&amp;nocache=1&amp;up_address=', addr, '" ',
 			'style="width:100%; height:100%;" ',
 			'frameborder="0" ',
 			'scrolling="no" ',
